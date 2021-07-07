4abf3ea208c636a29d3dc11460d7576490f3d2d2
hunter-33 <kurapica3b3@gmail.com>
2021-07-07 16:50:10 +0200

    first commit

---
+++ js/front.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    autoplay:true,
+    prevArrow:'<span type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></span>',
+    nextArrow:'<span type="button" class="slick-prev"><i class="fa fa-chevron-right"></i></span>',
+    dots: true,
+    arrows: true
+});
