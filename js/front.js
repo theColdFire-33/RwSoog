269ab7a80c8048a1d2159677fc1536364add0be6
theColdFire-33 <thehunter3b3@gmail.com>
2021-07-11 11:40:54 +0200

    save

--- js/front.js
+++ js/front.js
@@ -13,3 +13,65 @@ const list = document.querySelector('.cen-list');
 bar.onclick = function () {
     list.classList.toggle('open');
 }
+
+
+// jump to top onclick home button ..
+$("a[href='#home']").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 100);
+    return false;
+  });
+
+// scroll to top button ...**************************************************************************
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// *********************************************************************************************
+
+
+// jump to the section useing jQuery ... 
+// $('#myButton').click(function() {
+//    $.scrollTo($('#myDiv'), 1000);
+// });
+
+// navbar scroll to top and down ...
+// this code just for learn how to navbar work ...
+
+//  $(document).ready(function(){
+//       $(window).scroll(function() { // check if scroll event happened
+//         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+//           $(".navbar-fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+//         } else {
+//           $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+//         }
+//       });
+//     });
+
+//     $(document).ready(function(){
+//         $(window).scroll(function(){
+//             if($(window).scrollTop() > $(window).height()){
+//                 $(".menu").css({"background-color":"transparent"});   
+//             }
+//             else{
+//                 $(".menu").css({"background-color":"white"});
+//             }
+    
+//         })
+//     });
